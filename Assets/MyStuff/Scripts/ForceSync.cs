ba90c03b7d4568ea2a5de6329193d7670cddfdc0
Nick Halvorsen <nick@nickhalvorsen.com>
2019-09-17 19:11:42 -0400

    slight upgrade to clap physics

--- Assets/MyStuff/Scripts/ForceSync.cs
+++ Assets/MyStuff/Scripts/ForceSync.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class ForceSync : NetworkBehaviour
 {
-    public float clapEffectRadius = 4.0f;
-    public float clapEffectForce = 2.0f;
+    public const float ClapEffectRadius = 4.0f;
+    public const float ClapEffectForce = 25.0f;
 
     public void PlayForce()
     {
@@ -23,17 +23,35 @@ public class ForceSync : NetworkBehaviour
     void RpcTriggerForceOnClients()
     {
         Vector3 explosionPos = GetClapEffectOrigin();
-        Collider[] colliders = Physics.OverlapSphere(explosionPos, clapEffectRadius);
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, ClapEffectRadius);
 
         var me = GetComponent<Rigidbody>();
 
         foreach (Collider hit in colliders)
         {
-            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            Rigidbody hitPlayer = hit.GetComponent<Rigidbody>();
 
-            if (rb != null && rb.GetInstanceID() != me.GetInstanceID())
+            if (hitPlayer != null && hitPlayer.GetInstanceID() != me.GetInstanceID())
             {
-                rb.AddExplosionForce(clapEffectForce, explosionPos, clapEffectRadius, .5F, ForceMode.Impulse);
+                // old method, quite garbage
+                //hitPlayer.AddExplosionForce(clapEffectForce, explosionPos, clapEffectRadius, .5F, ForceMode.Impulse);
+
+                var distance = (hitPlayer.position - explosionPos).magnitude;
+                if (distance > ClapEffectRadius)
+                {
+                    continue;
+                }
+
+                // idk what this garbage is
+                //float forceMagnitude = (float)(ClapEffectForce * 3 / (4 * 3.14159 * distance * distance * distance));
+                float forceMagnitude = ClapEffectForce * (1.0f - (distance / ClapEffectRadius));
+                var horizontalForceDirection = (hitPlayer.position - explosionPos).normalized;
+                horizontalForceDirection.y = 0;
+
+                Debug.Log(forceMagnitude);
+
+                hitPlayer.AddForce(horizontalForceDirection * forceMagnitude, ForceMode.Impulse);
+                hitPlayer.AddForce(new Vector3(0, 1, 0) * forceMagnitude * 3, ForceMode.Impulse);
             }
         }
     }
