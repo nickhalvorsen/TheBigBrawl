c47ad50fd27c00426468cef5c75db749c4273cfa
Nick Halvorsen <lingosalad@gmail.com>
2019-10-05 21:20:03 -0400

    win sound and pickups assets

--- Assets/MyStuff/Scripts/PlayerGameRules.cs
+++ Assets/MyStuff/Scripts/PlayerGameRules.cs
@@ -71,6 +71,7 @@ public class PlayerGameRules : NetworkBehaviour
 
         GUI.Label(new Rect(10, 10, 200, 20), "Players in arena: " + _gameManager.PlayersInArena.ToString());
         GUI.Label(new Rect(10, 20, 200, 20), "Game state: " + _gameManager.GameState.ToString());
+        GUI.Label(new Rect(10, 180, 200, 20), "Gems: " + _money);
     }
 
     public void PickedUpMoney()
