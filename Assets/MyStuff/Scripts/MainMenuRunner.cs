02c0fbc9b131715a966c12e0b9260c3459d9b406
Nick Halvorsen <nick@nickhalvorsen.com>
2020-03-29 17:43:52 -0400

    title screen upgraded

---
+++ Assets/MyStuff/Scripts/MainMenuRunner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuRunner : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        _animator.SetFloat("InputVertical", 1.5f);
+        _animator.Play("Free Movement");
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * (150 * Time.deltaTime));
+
+    }
+}
