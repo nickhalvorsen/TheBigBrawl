43f5abe606379d9ee2ae9cfb813f800ebdd26adf
Nick Halvorsen <nick@hugemail.win>
2024-02-08 19:36:06 -0500

    fix mainmenudancer animation

--- Assets/MyStuff/Scripts/MainMenuRunner.cs
+++ Assets/MyStuff/Scripts/MainMenuRunner.cs
@@ -9,20 +9,12 @@ public class MainMenuRunner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _animator = GetComponent<Animator>();
 
-        if (_animator != null)
-        {
-            _animator.SetFloat("InputVertical", 1.5f);
-            _animator.Play("Free Movement");
-
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * (150 * Time.deltaTime));
-
     }
 }
